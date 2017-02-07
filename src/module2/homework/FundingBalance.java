95cd948c544499ffe999750ea3f5ebd18e527411
Natalia <mazurnata84@gmail.com>
2017-02-07 15:34:13 +0200

    some changes at task 2.4

--- src/module2/homework/FundingBalance.java
+++ src/module2/homework/FundingBalance.java
@@ -5,12 +5,12 @@ public class FundingBalance {
     static void withdrawBalance(String ownerName, String[] ownerNames, int[] balances, double withdrawal) {
         int balanceAfter = 0;
         for (int i = 0; i < ownerNames.length; i++) {
-            if (ownerName.equals(ownerNames[i]))
+            if (ownerName.equals (ownerNames[i]))
                 balanceAfter = (int)(balances[i] + withdrawal);
         }
         System.out.println(ownerName + " " + balanceAfter);
-
     }
+
     public static void main(String[] args) {
         int[] balances = {1200, 250, 2000, 500, 3200};
         String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
