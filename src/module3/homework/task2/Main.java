a27391617e47977527c36ddd23d7602129dc63f4
Natalia <mazurnata84@gmail.com>
2017-02-15 16:24:22 +0200

    commit

--- src/module3/homework/task2/Main.java
+++ src/module3/homework/task2/Main.java
@@ -2,8 +2,8 @@ package module3.homework.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Integer a = new Integer(2);
-        Integer b = new Integer(5);
+        Integer a = 2;
+        Integer b = 5;
 //        int a = 14;
 //        int b = 8;
 
