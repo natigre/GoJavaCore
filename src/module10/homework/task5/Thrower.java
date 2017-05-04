d84420582c7c9ba2321bb80f0d5dfab95ff184a2
Natalia <mazurnata84@gmail.com>
2017-05-04 09:46:56 +0300

    new commit

--- src/module10/homework/task5/Thrower.java
+++ src/module10/homework/task5/Thrower.java
@@ -2,6 +2,12 @@ package module10.homework.task5;
 
 public class Thrower {
     public void f() throws ExceptionExample1, ExceptionExample2, ExceptionExamlpe3 {
-        throw new ExceptionExample2();
+        if (true) {
+            throw new ExceptionExample1();
+        } if (true) {
+            throw new ExceptionExample2();
+        } else {
+            throw new ExceptionExamlpe3();
+        }
     }
 }
